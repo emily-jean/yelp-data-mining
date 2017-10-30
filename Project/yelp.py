8d31f650e6d320248a4f485d71332be3e4546f5f
vyshaal <vyshaal@ccs.neu.edu>
2017-10-30 19:20:18 -0400

    connection using pymysql

--- Project/yelp.py
+++ Project/yelp.py
@@ -4,18 +4,26 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 import pandas as pd
+import pymysql
 
 '''
     db_uri should be "dialect+driver://username:password@host:port/database"    
 '''
 db_uri = "mysql+pymysql://root:root@localhost:3306/yelp_db"
-engine = create_engine(db_uri)
-connection = engine.connect()
 
-inspector = inspect(engine)
-print("Tables in the database are " + str(inspector.get_table_names()))
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='yelp_db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+sql = "SELECT id,text FROM review limit 10"
+reviews = pd.read_sql(sql,connection).values
 
-reviews = pd.read_sql("SELECT id,text FROM review limit 10", connection).values
+# engine = create_engine(db_uri)
+# connection = engine.connect()
+
+# reviews = pd.read_sql("SELECT id,text FROM review limit 10", connection).values
 
 porterStemmer = PorterStemmer()
 wordLemmatizer = WordNetLemmatizer()
