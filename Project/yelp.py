d655b027b40e67931faea7dfb5c799d593fd56c8
vyshaal <vyshaal@ccs.neu.edu>
2017-10-31 19:18:46 -0400

    top 50 keywords in reviews

--- Project/yelp.py
+++ Project/yelp.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from sqlalchemy import (create_engine, Table, Column, Integer, String, MetaData, inspect)
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
@@ -13,6 +15,29 @@ import matplotlib.pyplot as plt
     pip install -U textblob
     python -m textblob.download_corpora
 '''
+
+
+def findFrequentWords(fwdf,sentiment,plotname):
+    fwdf = fwdf[(fwdf.sentiment == sentiment)]
+    all = fwdf['keywords'].tolist()
+    r = []
+    for wordlist in all:
+        parseList = [x for x in wordlist if not '.' in x]
+        parseList = [x for x in parseList if not "'" in x]
+        r.extend(parseList)
+    words = Counter(" ".join(r).split(" ")).most_common(50)
+    df = pd.DataFrame(words, columns=['Word', 'Count'])
+    df.plot.bar(x='Word', y='Count')
+    plt.savefig(plotname)
+
+
+def plotRatings(pdf,plotname):
+    ratingsdf = pdf.groupby('id')['sentiment', 'stars'].mean().head(pdf.shape[0])
+    ratingsdf.columns = ['Ratings from Sentiment Analysis', 'Ratings from Dataset']
+    # print(ratingsdf)
+    ratingsdf.plot.hist(alpha=0.5)
+    plt.savefig(plotname)
+
 
 db_uri = "mysql+pymysql://root:root@localhost:3306/yelp_db"
 
@@ -30,21 +55,18 @@ sql = """select top_biz.stars,top_biz.id,review.text FROM \
    ORDER BY review_count DESC \
    ) top_biz \
 INNER join review on top_biz.id = review.business_id \
+INNER join category on category.business_id = top_biz.id and category.category = 'Restaurants' \
 ORDER BY top_biz.review_count DESC;"""
 
-df = pd.read_sql(sql,connection)
-
+df = pd.read_sql(sql, connection)
+# blob = TextBlob("some random text where you!! extract intelligent key word with an, new boy and an rein deer dear")
+df['keywords'] = df.apply(lambda row: TextBlob(row.text).noun_phrases, axis=1)
 df['polarity'] = df.apply(lambda row: TextBlob(row.text).polarity,axis=1)
 df['sentiment'] = df.apply(lambda row: -5 if row.polarity < 0 else 5,axis=1)
-ratingsdf = df.groupby('id')['sentiment', 'stars'].mean().head(df.shape[0])
-ratingsdf.columns = ['Ratings from Sentiment Analysis', 'Ratings from Dataset']
-#print(ratingsdf)
 
-ratingsdf.plot.hist(alpha=0.5)
-plt.savefig('sentimentanalysis.png')
-
-
-
+findFrequentWords(df,-5,'top50_negativereviews')
+findFrequentWords(df,5,'top50_positivereviews')
+#plotRatings(df,'sentimentanalysis.png')
 
 
 
