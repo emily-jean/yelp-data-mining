9e923d0be77955153c16e2027a65e489849f0d9e
vyshaal <vyshaal@ccs.neu.edu>
2017-10-31 17:03:04 -0400

    sentiment analysis of all restaurant reviews within a city

--- Project/yelp.py
+++ Project/yelp.py
@@ -6,10 +6,14 @@ from nltk.tokenize import RegexpTokenizer
 import pandas as pd
 import pymysql
 from textblob import TextBlob
+import matplotlib.pyplot as plt
 
 '''
     db_uri should be "dialect+driver://username:password@host:port/database"    
+    pip install -U textblob
+    python -m textblob.download_corpora
 '''
+
 db_uri = "mysql+pymysql://root:root@localhost:3306/yelp_db"
 
 connection = pymysql.connect(host='localhost',
@@ -19,57 +23,28 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-sql = "SELECT review.id,review.text FROM review,business where review.business_id = business.id and business.city = 'Pittsburgh' limit 2;"
-reviews = pd.read_sql(sql,connection).values
-'''
-    pip install -U textblob
-    python -m textblob.download_corpora
-'''
-reviewsDict = {}
-for review in reviews:
-    reviewId,reviewText = review
-    blob = TextBlob(reviewText)
-    reviewsDict[reviewId] = blob
+sql = """select top_biz.stars,top_biz.id,review.text FROM \
+  (select * \
+   from business \
+   where business.review_count > 200 and business.city = 'Pittsburgh'\
+   ORDER BY review_count DESC \
+   ) top_biz \
+INNER join review on top_biz.id = review.business_id \
+ORDER BY top_biz.review_count DESC;"""
 
+df = pd.read_sql(sql,connection)
 
-for k,v in reviewsDict.items():
-    print("Review")
-    print(v.raw)
-    print("Review Id: ")
-    print(k)
-    print("Polarity: ")
-    print(v.polarity)
-    print("Words: ")
-    print(set(v.words)-set(stopwords.words('english')))
-    print("Keywords/nouns: ")
-    print(v.noun_phrases)
-    print()
+df['polarity'] = df.apply(lambda row: TextBlob(row.text).polarity,axis=1)
+df['sentiment'] = df.apply(lambda row: -5 if row.polarity < 0 else 5,axis=1)
+ratingsdf = df.groupby('id')['sentiment', 'stars'].mean().head(df.shape[0])
+ratingsdf.columns = ['Ratings from Sentiment Analysis', 'Ratings from Dataset']
+#print(ratingsdf)
+
+ratingsdf.plot.hist(alpha=0.5)
+plt.savefig('sentimentanalysis.png')
 
 
 
 
 
 
-# engine = create_engine(db_uri)
-# connection = engine.connect()
-
-# reviews = pd.read_sql("SELECT id,text FROM review limit 10", connection).values
-
-# porterStemmer = PorterStemmer()
-# wordLemmatizer = WordNetLemmatizer()
-# tokenizer = RegexpTokenizer(r'\w+')
-# reviews_dict = {}
-# for review in reviews:
-#     reviewId,reviewText = review
-#     reviewTokens = []
-#     ''' import nltk
-#         nltk.download('punkt')
-#         nltk.download('wordnet')
-#         nltk.download('stopwords')
-#     '''
-#     for token in tokenizer.tokenize(reviewText.lower()):
-#         if token not in stopwords.words('english'):
-#             reviewTokens.append(wordLemmatizer.lemmatize(porterStemmer.stem(token)))
-#     reviews_dict[reviewId] = reviewTokens
-#
-# print(reviews_dict)
